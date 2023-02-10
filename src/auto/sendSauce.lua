35f08d947642f46ab1984b0248dec705f48600f9
Defalts2 <Defalts2@outlook.com>
2023-02-10 07:02:32 -0300

    Fix Baraag links being sent only with new like character

--- src/auto/sendSauce.lua
+++ src/auto/sendSauce.lua
@@ -47,7 +47,9 @@ end
 local function getBaraagImageFromTheSecondLink(t)
   local baraagLink = {};
   for index, value in ipairs(t:split("\n")) do
-    if index > 1 then
+    if index == 2 then
+      table.insert(baraagLink, value);
+    elseif index > 2 then
       table.insert(baraagLink, '\n' .. value);
     end
   end
