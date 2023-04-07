9d6cd80c9b7e9dd270210299c9a1f6ae3f3643ee
matkijagenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2023-04-07 04:38:28 -0300

    Refactor code

--- src/commands/sauce.lua
+++ src/commands/sauce.lua
@@ -2,53 +2,61 @@ local limitHandler = require("../classes/sauce/LimitHandler");
 local imageHandler = require("../classes/sauce/ImageSenderHandler");
 require('discordia').extensions();
 
+local function findLinksToSend(message, link, limit, client)
+  coroutine.wrap(function()
+    if imageHandler.verify(link, imageHandler.doesNotRequireDownload) then
+
+      imageHandler.sendDirectImageUrl(link, message, limit);
+
+    elseif imageHandler.verify(link, imageHandler.requireDownload) then
+
+      imageHandler.downloadSendAndDeleteImages(link, message, limit);
+
+    elseif link:find("https://twitter.com/") then
+
+      if not imageHandler.sendTwitterDirectVideoUrl(link, message, limit) then
+        imageHandler.sendTwitterImages(link, message, limit, client);
+      end
+
+    end
+  end)();
+end
+
 local function sendSauce(message, client)
   local content = message.content;
-  if content then
-    if content:find("https://") then
-      content = content:gsub('\n', ' '):gsub('||', ' ');
-      local t = content:split(' ');
-      local limit = limitHandler.getRoomImageLimit(message) or 5;
+  if content and content:find("https://") then
+    content = content:gsub('\n', ' '):gsub('||', ' ');
+    local t = content:split(' ');
+    local limit = limitHandler.getRoomImageLimit(message) or 5;
 
-      if limit == 0 then return end
+    if limit == 0 then return end
 
-      for _, value in ipairs(t) do
-        if value ~= '' then
-          coroutine.wrap(function()
-            if imageHandler.verify(value, imageHandler.doesNotRequireDownload) then
-              imageHandler.sendDirectImageUrl(value, message, limit);
-            elseif imageHandler.verify(value, imageHandler.requireDownload) then
-              imageHandler.downloadSendAndDeleteImages(value, message, limit);
-            elseif value:find("https://twitter.com/") then
-              if not imageHandler.sendTwitterDirectVideoUrl(value, message, limit) then
-                imageHandler.sendTwitterImages(value, message, limit, client)
-              end
-            end
-          end)();
-        end
+    for _, link in ipairs(t) do
+      if link ~= '' then
+        findLinksToSend(message, link, limit, client);
       end
     end
+
   end
 end
 
 local function sendAnySauce(message)
   local content = message.content;
-  if content then
-    if content:find("https://") then
-      content = content:gsub('\n', ' '):gsub('||', ' ');
-      local t = content:split(' ');
-      local limit = limitHandler.getRoomImageLimit(message) or 5;
+  if content and content:find("https://") then
+    content = content:gsub('\n', ' '):gsub('||', ' ');
+    local t = content:split(' ');
+    local limit = limitHandler.getRoomImageLimit(message) or 5;
 
-      if limit == 0 then return end
+    if limit == 0 then return end
 
-      for _, value in ipairs(t) do
-        if value ~= '' and value:find("https://") then
-          coroutine.wrap(function()
-            imageHandler.downloadSendAndDeleteImages(value, message, limit);
-          end)();
-        end
+    for _, link in ipairs(t) do
+      if link ~= '' and link:find("https://") then
+        coroutine.wrap(function()
+          imageHandler.downloadSendAndDeleteImages(link, message, limit);
+        end)();
       end
     end
+
   end
 end
 
@@ -59,8 +67,8 @@ return {
           tools.subCommand("channel", "Sets a limit for this channel only nanora!")
           :addOption(
             tools.integer("limit", "Default is 5 nanora! Input 0 if you don't want me to send images again nora!")
-            :setMinValue(0)
-            :setMaxValue(10)
+            :setMinlink(0)
+            :setMaxlink(10)
             :setRequired(true)
           )
         )
@@ -68,8 +76,8 @@ return {
           tools.subCommand("global", "Sets a limit for this entire server nanora!")
             :addOption(
               tools.integer("limit", "Default is 5 nanora! Input 0 if you don't want me to send images again nora!")
-              :setMinValue(0)
-              :setMaxValue(10)
+              :setMinlink(0)
+              :setMaxlink(10)
               :setRequired(true)
             )
         )
