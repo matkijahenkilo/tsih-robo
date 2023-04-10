6db7a145d9f93681d5aa240fdc94651c0c38a05c
matkijagenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2023-04-10 08:34:34 -0300

    Refactor random reactor

--- src/data/settings.lua
+++ src/data/settings.lua
@@ -1,7 +1,10 @@
 return {
   prefix = "ts!",
+  shouldResetCommands = false,
   emoticonsServers = {
     "579432155546124306",
+    "338828546468478977",
+    "705450725513363467",
+    "1028009503670685777",
   },
-  shouldResetCommands = false,
 }
--- src/utils/randomReact.lua
+++ src/utils/randomReact.lua
@@ -1,7 +1,8 @@
 local M = {};
 
-function M.sendRandomReaction(message, emoticonServer)
-    message:addReaction(emoticonServer[math.random(#emoticonServer)].emojis:random());
+function M.sendRandomReaction(message, guild)
+    local customEmoji = guild[math.random(#guild)].emojis:random();
+    message:addReaction(customEmoji);
 end
 
 return M;
