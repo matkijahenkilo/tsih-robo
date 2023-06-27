883ad5e715734396e3de0f4ca4c2487d511b3e22
matkijahenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2023-06-27 01:09:49 -0300

    Add check for non existent files

--- src/commands/sauce/gallerydl.lua
+++ src/commands/sauce/gallerydl.lua
@@ -9,6 +9,20 @@ local function getFileSizeInMegaBytes(file)
   return fs.statSync(file).size / (1024*1024)
 end
 
+local function fileExists(file)
+  return fs.statSync(file) ~= nil
+end
+
+local function fillNewTable(t)
+  local newTable = {};
+  for _, value in ipairs(t) do
+    if value ~= '' then
+      table.insert(newTable, value);
+    end
+  end
+  return newTable
+end
+
 local function getSpecificLinksFromString(t, string)
   local specificLinks = {};
   for _, value in ipairs(t:split('\n')) do
@@ -19,42 +33,47 @@ local function getSpecificLinksFromString(t, string)
   return specificLinks;
 end
 
+local function getBaraagLinks(link)
+  local baraagLinks = getSpecificLinksFromString(link, "media.baraag.net");
+  table.remove(baraagLinks, 1); --removes the first, already embedded image
+  return baraagLinks;
+end
+
 local function readProcess(child)
-  local images = {};
   child:waitExit();
   local link = child.stdout.read();
 
-  table.insert(images, link);
   if link then
     if link:find("pbs.twimg") then
       return getSpecificLinksFromString(link, "video.twimg");
     elseif link:find("media.baraag.net") then
-      local baraagLinks = getSpecificLinksFromString(link, "media.baraag.net");
-      table.remove(baraagLinks, 1); --removes the first, already embedded image
-      return baraagLinks;
+      return getBaraagLinks(link)
     end
   end
 
-  return images;
+  return { link }
 end
 
-local function filterLargeFiles(downloadedFiles)
-  local filestbl = downloadedFiles:split("\n");
-  for index, file in ipairs(filestbl) do
+local function filterLargeFiles(existingFilestbl)
+  for index, file in ipairs(existingFilestbl) do
     if file ~= '' and getFileSizeInMegaBytes(file) >= MAX_UPLOAD_LIMIT then
       fs.unlinkSync(file);
-      filestbl[index] = "";
+      existingFilestbl[index] = nil;
     end
   end
 
-  local filteredFilestbl = {};
-  for _, file in ipairs(filestbl) do
-    if file ~= '' then
-      table.insert(filteredFilestbl, file);
+  return fillNewTable(existingFilestbl)
+end
+
+local function filterNilFiles(downloadedFiles)
+  local filestbl = downloadedFiles:split("\n");
+  for index, file in ipairs(filestbl) do
+    if file ~= '' and not fileExists(file) then
+      filestbl[index] = nil;
     end
   end
 
-  return filteredFilestbl;
+  return fillNewTable(filestbl);
 end
 
 function gallerydl.downloadImage(link, id, limit)
@@ -66,13 +85,14 @@ function gallerydl.downloadImage(link, id, limit)
       link
     }
   });
-
+  -- obviously not the best idea to concat table, transform to table, concat table and so on...
   local downloadedFiles = table.concat(readProcess(child));
   downloadedFiles = downloadedFiles:gsub("# ", ''):gsub("\r", '');
 
-  local filestbl = filterLargeFiles(downloadedFiles);
+  local existingFilestbl = filterNilFiles(downloadedFiles)
+  local filesToSend = filterLargeFiles(existingFilestbl);
 
-  return filestbl;
+  return filesToSend;
 end
 
 function gallerydl.getUrl(url, limit)
