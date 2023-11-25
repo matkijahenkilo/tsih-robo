7556b891e03c90c72c303e08ff764f8e42230f90
matkijahenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2023-11-25 07:25:48 -0300

    Organize command's logs

--- src/commands/randomemoji/init.lua
+++ src/commands/randomemoji/init.lua
@@ -12,7 +12,7 @@ return {
   getSlashCommand = function(tools)
     return tools.slashCommand("randomemoji", "Do you allow me to use this server's emojis to random react messages nora? ")
         :addOption(
-          tools.boolean("allow", "If you set true, I'll be able to randomly use your server's emoji nora! Otherwise I'll not!")
+          tools.boolean("allow", "If you set true, I'll be able to randomly use your server's emojis nora! Otherwise I'll not!")
           :setRequired(true)
         )
   end,
@@ -28,40 +28,37 @@ return {
       if emojiHandler.removeServer(interaction.guild.id) then
         interaction:reply("How can I now annoy Nanako in other servers nanora!?")
       else
-        interaction:reply("But your server is not even saved nanora!")
+        interaction:reply("But your server is not even saved nanora!", true)
       end
     end
   end,
 
   execute = function(message, client)
-    local server, id = nil, nil
+    local guild, id = nil, nil
     local limit = 0
     local emoji
 
     repeat
       repeat
-        server, id = getRandomServer(client)
+        guild, id = getRandomServer(client)
         if limit >= 100 then
-          logger:log(2, "Emoji fetch limit just hit 100. Last guild id fetched: %s", id)
+          logger:log(2, "randomemoji: fetch limit hit 100. Last guild id fetched: %s", id)
           return
         end
         limit = limit + 1
-      until server and server.emojis
-      emoji = server.emojis:random()
+      until guild and guild.emojis
+      emoji = guild.emojis:random()
     until emoji
 
     local ok = message:addReaction(emoji)
 
     if ok then
-      logger:log(3, "emoji : '%s' to %s in '%s', '%s' (took %s tries)",
+      logger:log(3, "randomemoji: sent emoji '%s' after %s retries",
         emoji.name,
-        message.author.name,
-        message.guild.name,
-        message.channel.name,
         limit
       )
     else
-      logger:log(1, "emoji : Failed to send emoji '%s'", emoji.name)
+      logger:log(1, "randomemoji: Failed to send emoji '%s'", emoji.name)
     end
   end
 }
